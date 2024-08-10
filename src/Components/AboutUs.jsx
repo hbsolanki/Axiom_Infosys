c5d7fb011e5ca61fd7c61373f1938355e1abc906
shahjaynish <149518797+shahjaynish@users.noreply.github.com>
2024-08-10 18:27:27 +0530

    About Us create

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -4,7 +4,18 @@ function AboutUs() {
   return (
     <>
       <div className="main-aboutus">
-        <div className="map-display"></div>
+        <div className="map-display">
+          <p><b>Axiom Infosys</b> was started by <b>Mr. Bhavesh Shah</b> in 2002.
+          <b>Axiom Infosys</b> is a service provider for all <b>Laptops , PC'S , Printers and CCTV Cameras</b> of any Brand.
+          Here in <b>Axiom Infosys</b> we are commited to provide the <b>best service</b> and the <b>best and Genuine</b>
+          parts of all the devices. Now we stand as <b>Symbol</b> of <b>Trust</b> and <b>Quality</b>.
+          And more than that we provide the best and fast solution for our clients.Our Average time for a device 
+          repairing and solving issues <b> Min time less than 5 hrs</b> and <b>Max time is 48 hrs</b>.
+          <b> Axiom Infosys </b> has an <b>Experience</b> of <b>20+ years </b>in this field and has served <b>5000+ Customers</b>.</p>
+          <p><b>Mr. Bhavesh Shah</b> himself is <b>Degree Engineer</b> and holds the Degree in the field of <b>EC (Electronics and Communications) </b>
+          and all our staff includes <b>Well Trained Proffesionals and Degree Engineers</b>.</p>
+
+        </div>
       </div>
     </>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,13 +8,13 @@ function Home() {
         <h1>Axiom Infosys</h1>
         <p className="par">
           We are Sales and Service Providers of all
-          <b>Laptops</b> ,<b>Desktops</b>, <b>CCTV Cameras </b>,<b>Printers</b>
+          <b> Laptops</b> ,<b>Desktops</b>, <b>CCTV Cameras </b>,<b>Printers</b>
           and <b>All Computer/Laptop Accessories.</b>
           <br />
           Also We are <b>Wholesellers</b> of <b>CCTV Cameras</b> and
           <b>SSD's</b>.<br />
           We have all <b>Degree Engineers</b> for work.Also we hold
-          <b> m Mastery</b> in <b>Laptop/Desktop</b> Repairing. <br />
+          <b> Mastery</b> in <b>Laptop/Desktop</b> Repairing. <br />
           We are located at GF-3, Cascade Complex Opp. Mota Appartment, beside
           Raju Pan Corner, Chhani Jakatnaka, Vadodara, Gujarat - 390002 <br />
           <b>Contact Number: 9898360081</b>
